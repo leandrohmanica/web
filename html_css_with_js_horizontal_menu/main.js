0117b07674e20bddb6eb309ffd36e3fee92bc6ef
Leandro H. Manica <leandrohmanica@gmail.com>
2015-11-12 22:59:34 -0200

    Add simple javascript.

---
+++ html_css_with_js_horizontal_menu/main.js
@@ -0,0 +1,31 @@
+// Event triggered when all content is loaded
+// include images (even big ones)
+window.addEventListener('load', function () {
+  console.log('window loaded');
+})
+
+// Event triggered when the DOM (document) is loaded
+// Similar to jQuery ready event
+document.addEventListener('DOMContentLoaded', function () {
+  var menuItems = document.getElementById('menu')
+                          .children[0] // UL
+                          .children;   // LI's
+  for(var i=0; i<menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    var subMenu = menuItem.children[1];
+    if(subMenu) {
+      // Show submenu on mouse over
+      menuItem.addEventListener('mouseover', function (e) {
+        // this -> li
+        // e.srcElement -> a
+        e.srcElement.nextElementSibling.style.display = 'block';
+      });
+      // Hide submenu on mouse leave
+      menuItem.addEventListener('mouseleave', function (e) {
+        var subMenu = e.srcElement.children[1];
+        if(subMenu.style.display != 'none')
+          subMenu.style.display = 'none';
+      });
+    }
+  }
+});
